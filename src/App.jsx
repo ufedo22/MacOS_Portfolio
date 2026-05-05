fa79fc41a8ef6cb9988654e6ac783b6dfb4fddaf
joseph sunday <josephluckysunday@gmail.com>
2026-05-05 01:03:38 +0100

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Welcome } from "#components";
+import { Dock, Navbar, Welcome } from "#components";
 import React from "react";
 
 const App = () => {
@@ -6,6 +6,7 @@ const App = () => {
     <main>
       <Navbar />
       <Welcome />
+      <Dock/>
     </main>
   );
 };
